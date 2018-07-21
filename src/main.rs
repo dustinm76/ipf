6b1bb353fa65a59a75bd367b76304783fb123e74
Dustin Mackenzie <dustin.mackenzie76@gmail.com>
2018-07-21 17:06:26 -0600

    path_parser

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 mod options;
+mod path_parser;
+
+use std::env;
+use std::io;
 
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut command_line_paths = env::args();
+    command_line_paths.next(); // Pop program name off
+
+    let paths = path_parser::parse(command_line_paths, stdin.lock());
+
+    for path in paths {
+        println!("{}", path);
+    }
 }
 
---
+++ src/path_parser.rs
@@ -0,0 +1,25 @@
+use std::env;
+use std::io;
+use std::io::Read;
+
+pub fn parse(args: env::Args, mut stdin: io::StdinLock) -> Vec<String> {
+    let mut paths: Vec<String> = Vec::new();
+
+    for argument in args {
+        paths.push(argument);
+    }
+
+    if paths.is_empty() {
+        let mut bytes: Vec<u8> = Vec::new();
+        stdin.read_to_end(&mut bytes);
+
+        let input = String::from_utf8(bytes).unwrap();
+        let lines = input.split("\n"); //TODO if null-input, split on \0
+
+        for line in lines {
+            paths.push(line.to_string());
+        }
+    }
+
+    return paths;
+}
